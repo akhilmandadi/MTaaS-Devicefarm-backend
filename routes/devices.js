d483fb3f239209ab6e39d876fcb207d0a3feae1d
Akhil Reddy <akhilreddymandadi@gmail.com>
2020-05-03 00:15:54 -0700

    Pre Booking

--- routes/devices.js
+++ routes/devices.js
@@ -6,5 +6,9 @@ router.get('/ondemand', devices.getAvailableOnDemandDevices);
 router.post('/ondemand', devices.createOnDemandDevice);
 router.get('/prebook', devices.getAvailablePreBookDevices);
 router.post('/prebook', devices.createPreBookDevice);
+router.post('/project/:id/devices', devices.createPreBookDevice);
+router.get('/project/:id/devices', devices.getAvailablePreBookDevices);
+router.post('/project/:id/emulators', devices.createPreBookEmulator);
+router.get('/project/:id/emulators', devices.getAvailablePreBookEmulators);
 
 module.exports = router;
